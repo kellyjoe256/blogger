e64628569464565116a4b78907c1f650a146557f
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-07 17:32:01 +0300

    modified users table to match and support specs provided

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->integer('id', true, true);
+            $table->string('name', 100);
+            $table->string('username', 50)->unique();
+            $table->string('email', 50)
+                ->unique()
+                ->index('ix_email');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 150);
+            $table->boolean('super_user')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
