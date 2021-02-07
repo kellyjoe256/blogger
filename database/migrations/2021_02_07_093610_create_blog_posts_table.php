73502243862bea08463079efc2b2e1dc68ad403b
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-07 17:18:12 +0300

    blog posts table migration

---
+++ database/migrations/2021_02_07_093610_create_blog_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->integer('id', true, true);
+            $table->integer('user_id', false, true)
+                ->nullable();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->dateTime('publication_date')
+                ->index('ix_publication_date');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
