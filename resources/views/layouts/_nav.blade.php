e31164bb58a2736cf39886627fcef66881c9bfb3
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-07 17:33:27 +0300

    created separate nav file and included in base layout file

---
+++ resources/views/layouts/_nav.blade.php
@@ -0,0 +1,28 @@
+<nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+    @guest
+        <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
+        @if (Route::has('register'))
+            <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
+        @endif
+    @else
+        <a
+            href="{{ route('dashboard') }}"
+            class="text-gray-50 no-underline hover:underline"
+        >{{ __('Dashboard') }}</a>
+
+        <a
+            href="#"
+            class="text-gray-50 no-underline hover:underline"
+        >{{ __('Posts') }}</a>
+
+        <span>{{ Auth::user()->name }}</span>
+
+        <a href="{{ route('logout') }}"
+           class="no-underline hover:underline"
+           onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+            {{ csrf_field() }}
+        </form>
+    @endguest
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,24 +24,7 @@
                         Blogger
                     </a>
                 </div>
-                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
-                    @guest
-                        <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        @if (Route::has('register'))
-                            <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        @endif
-                    @else
-                        <span>{{ Auth::user()->name }}</span>
-
-                        <a href="{{ route('logout') }}"
-                           class="no-underline hover:underline"
-                           onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                            {{ csrf_field() }}
-                        </form>
-                    @endguest
-                </nav>
+                @include('layouts._nav')
             </div>
         </header>
 
