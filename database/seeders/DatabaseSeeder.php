13b5f6136fe168209438abc272a7945a47b96e8a
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-07 17:30:16 +0300

    seed admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::factory(1)->create([
+             'username' => 'admin',
+             'email' => 'admin@example.com',
+             'super_user' => true,
+         ]);
     }
 }
