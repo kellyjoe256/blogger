f2ef66abe3cc16fff37b1be595906b17aa4bf241
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-08 08:33:19 +0300

    removed unnecessary login and register routes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,6 @@
 
 @section('content')
     <div class="flex flex-col">
-        @if(Route::has('login'))
-            <div class="absolute top-0 right-0 mt-4 mr-4 space-x-4 sm:mt-6 sm:mr-6 sm:space-x-6">
-                @auth
-                    <a href="{{ route('dashboard') }}" class="no-underline
-                hover:underline text-sm
-                 font-normal text-teal-800 uppercase">{{ __('Home') }}</a>
-                @else
-                    <a href="{{ route('login') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase">{{ __('Login') }}</a>
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase">{{ __('Register') }}</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-
         <div class="min-h-screen flex items-center justify-center">
             <div class="flex flex-col justify-around h-full">
                 <div>
