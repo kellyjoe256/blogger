841c068d458623ee35ba27043e990746ac603d94
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-08 08:36:16 +0300

    added ability to import posts on dashboard for admins

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,17 +5,29 @@
 @section('content')
 <main class="sm:container sm:mx-auto sm:mt-10">
     <div class="w-full sm:px-6">
-
-        @if (session('status'))
-            <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
-
         <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
-            <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
-                Dashboard
+            <header class="font-semibold bg-gray-200 text-gray-700 py-3 px-6
+            sm:py-6 sm:px-8 sm:rounded-t-md">
+                <div class="flex justify-between items-center" x-data="{}">
+                    <span>Dashboard</span>
+
+                    @if(Auth::user()->admin)
+                        <a href="#"
+                           class="no-underline hover:no-underline
+                       hover:text-white flex items-center justify-center
+                       bg-blue-800 outline-none hover:bg-blue-700
+                       focus:border-blue-900 text-white font-semibold px-4 py-2
+                       rounded shadow"
+                           x-on:click.prevent="document.getElementById('import-posts-form').submit()"
+                        >Import posts</a>
+                        <form id="import-posts-form"
+                              action="{{ route('dashboard.import_posts') }}"
+                              method="POST" class="hidden">
+                            {{ csrf_field() }}
+                        </form>
+                    @endif
+                </div>
             </header>
 
             <div class="w-full p-6">
