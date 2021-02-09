eead4e60b4db721c064eeba0830c1ef56e8aa951
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-09 10:48:53 +0300

    allow for deletion of blog post cache key on update and deletion of blog post

--- app/Observers/BlogPostObserver.php
+++ app/Observers/BlogPostObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\BlogPost;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class BlogPostObserver
 {
@@ -13,12 +14,39 @@ class BlogPostObserver
      * @param BlogPost $blogPost
      * @return void
      */
-    public function creating(BlogPost $blogPost)
+    public function creating(BlogPost $blogPost): void
     {
         $publication_date = $blogPost->publication_date;
 
         $blogPost->title = ucfirst($blogPost->title);
         $blogPost->description = ucfirst($blogPost->description);
         $blogPost->publication_date = $publication_date ?: Carbon::now();
+    }
+
+    /**
+     * Handle the BlogPost "updating" event.
+     *
+     * @param BlogPost $blogPost
+     * @return void
+     */
+    public function updating(BlogPost $blogPost): void
+    {
+        $this->deleteBlogPostCacheKey($blogPost);
+    }
+
+    /**
+     * Handle the BlogPost "deleting" event.
+     *
+     * @param BlogPost $blogPost
+     * @return void
+     */
+    public function deleting(BlogPost $blogPost): void
+    {
+        $this->deleteBlogPostCacheKey($blogPost);
+    }
+
+    private function deleteBlogPostCacheKey(BlogPost $blogPost): void
+    {
+        Cache::delete("blog_post." . $blogPost->id);
     }
 }
