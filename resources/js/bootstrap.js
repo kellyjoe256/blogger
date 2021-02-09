fd7735020fbea7297db343eb90122b6e540a0d69
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-09 10:58:50 +0300

    registered global window function to provide the ability to add sort string to the query string

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,1 +1,17 @@
 require('alpinejs');
+const qs = require('qs')
+
+window.sorter = function (sortElement) {
+    if (!sortElement) {
+        return;
+    }
+    const name = sortElement.getAttribute('name');
+    const value = sortElement.value;
+    const location = window.location;
+    const search = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    });
+    // add new query param
+    search[name] = value;
+    window.location.href = `${location.origin}${location.pathname}?${qs.stringify(search)}`;
+}
