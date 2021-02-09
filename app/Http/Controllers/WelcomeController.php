f053e7a3293c5fef9fd0cee89e7c1939ba4344e5
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-09 11:09:09 +0300

    added methods for showing all posts and a single post

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\BlogPost\BlogPostRepository;
+
 class WelcomeController extends Controller
 {
+    /**
+     * @var BlogPostRepository
+     */
+    private $blogPostRepository;
+
+    public function __construct(BlogPostRepository $blogPostRepository)
+    {
+        $this->blogPostRepository = $blogPostRepository;
+    }
+
     public function index()
     {
-        return view('welcome');
+        $sort_direction = publication_date_sort_direction(request('sort', ''));
+        $blog_posts = $this->blogPostRepository->blogPosts($sort_direction);
+
+        return view('welcome', compact('blog_posts'));
+    }
+
+    public function show($id)
+    {
+        $blog_post = $this->blogPostRepository->blogPost($id);
+
+        return view('show_blog_post', compact('blog_post'));
     }
 }
