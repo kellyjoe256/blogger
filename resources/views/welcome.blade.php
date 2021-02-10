3e4af91ca12e84bd1d6b747b20c303f0e3ec64fe
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2021-02-10 11:28:59 +0300

    show message when no posts are available

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,29 +2,39 @@
 
 @section('content')
     <div class="container w-full md:max-w-3xl mx-auto pt-20">
-        @include('includes.common.sorter')
+        @if ($blog_posts->count())
+            @include('includes.common.sorter')
 
-        @foreach($blog_posts as $blog_post)
-            <div class="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
-                <!--Title-->
-                <div class="font-sans">
-                    <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">
-                        <a href="{{ route('show_blog_post', $blog_post->id)}}">
-                            {{ $blog_post->title }}
-                        </a>
-                    </h1>
-                    <p class="text-sm md:text-base font-normal text-gray-600">
-                        Published on
-                        <span>{{ optional
+            @foreach($blog_posts as $blog_post)
+                <div class="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
+                    <!--Title-->
+                    <div class="font-sans">
+                        <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">
+                            <a href="{{ route('show_blog_post', $blog_post->id)}}">
+                                {{ $blog_post->title }}
+                            </a>
+                        </h1>
+                        <p class="text-sm md:text-base font-normal text-gray-600">
+                            Published on
+                            <span>{{ optional
                         ($blog_post->publication_date)
                         ->format('F j, Y') }}</span>
-                    </p>
+                        </p>
+                    </div>
+
+                    <p class="py-6">{{ $blog_post->blogPostIntro() }}</p>
                 </div>
+            @endforeach
 
-                <p class="py-6">{{ $blog_post->blogPostIntro() }}</p>
+            {{ $blog_posts->appends($_GET)->links('includes.pagination.simple') }}
+        @else
+            <div class="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal" style="font-family:Georgia,serif;">
+                <div class="font-sans">
+                    <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">
+                        No posts available
+                    </h1>
+                </div>
             </div>
-        @endforeach
-
-        {{ $blog_posts->appends($_GET)->links('includes.pagination.simple') }}
+        @endif
     </div>
 @endsection
